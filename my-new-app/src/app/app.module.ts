5b56a08c2f579399b98cac8baa182844cf93977b
Greg Reed <dragonmist12@gmail.com>
2017-10-02 16:37:24 -0500

    created a username component - 2 way binding and string interpolation

--- my-new-app/src/app/app.module.ts
+++ my-new-app/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AlertWarningComponent } from './alert-warning/alert-warning.component';
 import { AlertSuccessComponent } from './alert-success/alert-success.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlertWarningComponent,
-    AlertSuccessComponent
+    AlertSuccessComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
