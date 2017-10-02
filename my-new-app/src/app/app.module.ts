7cbda8a1b1b2b4d3ddccd8ccc21a6d5e4c5de869
Greg Reed <dragonmist12@gmail.com>
2017-10-02 11:38:43 -0500

    Created two new components, warning and success alerts, and styled them.

--- my-new-app/src/app/app.module.ts
+++ my-new-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlertWarningComponent } from './alert-warning/alert-warning.component';
+import { AlertSuccessComponent } from './alert-success/alert-success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertWarningComponent,
+    AlertSuccessComponent
   ],
   imports: [
     BrowserModule
