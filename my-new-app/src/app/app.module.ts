1f397d95b697a8c8c80b670f38e2ad6b544bc57b
Greg Reed <dragonmist12@gmail.com>
2017-10-03 13:57:57 -0500

    updated app.module.ts file

--- my-new-app/src/app/app.module.ts
+++ my-new-app/src/app/app.module.ts
@@ -6,15 +6,13 @@ import { AppComponent } from './app.component';
 import { AlertWarningComponent } from './alert-warning/alert-warning.component';
 import { AlertSuccessComponent } from './alert-success/alert-success.component';
 import { FormComponent } from './form/form.component';
-import { Assignment3Component } from './assignment3/assignment3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlertWarningComponent,
     AlertSuccessComponent,
-    FormComponent,
-    Assignment3Component
+    FormComponent
   ],
   imports: [
     BrowserModule,
@@ -23,4 +21,5 @@ import { Assignment3Component } from './assignment3/assignment3.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
